d65a9d10afdd93a3b4781ff9edf28de5d33efca9
alvalenda <banysan@msn.com>
2023-02-16 19:43:08 -0300

    typeorm

--- src/common/config/config-constants.ts
+++ src/common/config/config-constants.ts
@@ -2,7 +2,14 @@ export const server = {
   port: 3333,
 };
 
-export const database = {
+export const database: Database = {
+  type: 'postgres',
   host: 'localhost',
   port: 5432,
+};
+
+type Database = {
+  type: 'postgres';
+  host: string;
+  port: number | string;
 };
