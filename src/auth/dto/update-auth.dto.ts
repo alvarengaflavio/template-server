aee5f93e6f91c003ea75bbbcf921ef6aa12a9ad0
Flávio Alvarenga <13731643+alvalenda@users.noreply.github.com>
2023-04-04 11:23:01 -0300

    Delete src directory

--- src/auth/dto/update-auth.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { AuthCredentialsDto } from './auth-credentials.dto';
-
-export class UpdateAuthDto extends PartialType(AuthCredentialsDto) {}
