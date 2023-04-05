fd6896abc15a3ddc5877ec0f5ae15608708b3e9b
Flavio Alvarenga <flavio.alva@outlook.com>
2023-04-05 13:16:17 -0300

    feat(helper): implement `typeORM` error helper

---
+++ src/common/helpers/type-orm-error.helper.ts
@@ -0,0 +1,22 @@
+import { Error } from '../types/types';
+import { nestErrorHelper } from './nest-error.helper';
+
+export function typeOrmExceptionHelper(err: Error) {
+  switch (err?.code) {
+    case '23505':
+      nestErrorHelper({
+        name: 'UnprocessableEntityError',
+        message: err?.detail ?? err.message ?? 'Unique constraint failed',
+      });
+
+      break;
+
+    default:
+      nestErrorHelper({
+        name: err.name ?? 'InternalServerError',
+        message: err?.detail ?? err.message ?? 'Internal server error',
+      });
+
+      break;
+  }
+}
